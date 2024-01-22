7ab557e3fe31c5c2a01aecbb727ec8c4f897e0b9
Nicola <ngrassini@yahoo.it>
2024-01-22 14:28:00 +0000

    • crate basic Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return (
+    <div style={{ color: "slategrey" }}>
+      <p>Loading data.</p>
+      <p>Please wait, this may take a few seconds...</p>
+    </div>
+  );
+}

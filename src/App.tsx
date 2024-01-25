bbe8829d3ca32cc29cd31e360a3af526f2bc5b1e
Nicola <ngrassini@yahoo.it>
2024-01-25 10:02:41 +0000

    • import MapChart into App.tsx • render it for four areas • remove effect with loadData from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,37 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import { csv, DSVRowArray } from "d3";
-import Loader from "./components/Loader";
+import MapChart from "./components/MapChart";
 
 /** handling production and development:
  * place data in /public folder
- * name env variable REACT_APP_PUBLIC_URL
- * enable PUBLIC_URL in .env only at build 
+ * set PUBLIC_URL in build script
  * */
-const DATA_ADDRESS: string = "./data/dft_traffic_counts_aadf_by_direction.csv";
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [data, setData] = useState<DSVRowArray<string>>();
-
-  useEffect(() => {
-    const getData = async () => {
-      setLoading(true);
-      try {
-        /* d3.csv uses d3.fetch which returns a promise */
-        const trafficData = await csv(DATA_ADDRESS);
-        // console.log(trafficData[0]);
-        setData(trafficData);
-      } catch (e) {
-        console.log(e);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
-  }, []);
-
+  console.log("App render");
   return (
-    <div className="App">
-      <header>
-        <h3>Traffic app: built with TS, React and D3 by kinolag || NicolaG</h3>
+    <div className="centeredColumn">
+      <header className="txt-c radius-8">
+        <h3>Traffic app: built with TS, React and D3</h3>
+        <p>
+          developed by{" "}
+          <a
+            className="a1"
+            href="https://github.com/kinolag"
+            target="_blank"
+            rel="noreferrer"
+            title="Developer's GitHub profile"
+          >
+            kinolag
+          </a>{" "}
+          | NicolaG (01/2024)
+        </p>
       </header>
-      {loading && <Loader />}
-      {data && data.length > 0 && (
-        <>
-          <p>
-            {data[0].Latitude} {data[0].Longitude}
-          </p>
-          <p>
-            {data.reverse()[0].Latitude}
-            {data.reverse()[0].Longitude}
-          </p>
-        </>
-      )}
+      <main>
+        <MapChart area="towerHamlets" />
+        <MapChart area="southwark" />
+        <MapChart area="lewisham" />
+        <MapChart area="greenwich" />
+      </main>
     </div>
   );
 }

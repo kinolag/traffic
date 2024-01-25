34f87d16f452175ae48cfa1b633fd3be53cfcf20
Nicola <ngrassini@yahoo.it>
2024-01-25 10:07:44 +0000

    • extract Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
 import "./App.css";
+import Header from "./components/Header";
 import MapChart from "./components/MapChart";
 
 /** handling production and development:
  * place data in /public folder
  * set PUBLIC_URL in build script
+ * (or enable PUBLIC_URL in .env only at build)
  * */
+
+// export type Areas = Array<AvailableArea>;
+
+// const AREAS: Areas = [
+//   { value: "greenwich", label: "Greenwich" },
+//   { value: "lewisham", label: "Lewisham" },
+//   { value: "southwark", label: "Southwark" },
+//   { value: "towerHamlets", label: "Tower Hamlets" },
+// ];
 
 function App() {
   console.log("App render");
   return (
     <div className="centeredColumn">
-      <header className="txt-c radius-8">
-        <h3>Traffic app: built with TS, React and D3</h3>
-        <p>
-          developed by{" "}
-          <a
-            className="a1"
-            href="https://github.com/kinolag"
-            target="_blank"
-            rel="noreferrer"
-            title="Developer's GitHub profile"
-          >
-            kinolag
-          </a>{" "}
-          | NicolaG (01/2024)
-        </p>
-      </header>
+      <Header/>
       <main>
+        {/* <MapChart area="gb" /> */}
         <MapChart area="towerHamlets" />
         <MapChart area="southwark" />
         <MapChart area="lewisham" />
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <header className="txt-c radius-8">
+      <h3>Traffic app: built with TS, React and D3</h3>
+      <p>
+        developed by{" "}
+        <a
+          className="a1"
+          href="https://github.com/kinolag"
+          target="_blank"
+          rel="noreferrer"
+          title="Developer's GitHub profile"
+        >
+          kinolag
+        </a>{" "}
+        | NicolaG (01/2024)
+      </p>
+    </header>
+  );
+}

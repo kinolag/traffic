5771509c7009d4702d75da9842d56c0d41cebb19
Nicola <ngrassini@yahoo.it>
2024-01-27 10:04:50 +0000

    • improve text of chart legend

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -8,20 +8,22 @@ export default function Legend({ chartType }: LegendProps) {
       <div className="txt-c txt-14">
         <h3 className="is-dsg">Chart Legend</h3>
         <p>
-          • Elements represent the{" "}
+          • <strong className="is-dsb">Parameters</strong>: Elements represent
+          the <strong className="is-dsb">number of vehicles</strong> recorded at
+          a traffic point{" "}
           <strong className="is-dsb">
-            number of vehicles (per selected type)
-          </strong>{" "}
-          recorded at a traffic point{" "}
-          <strong className="is-dsb">in the selected year</strong>.
+            {" "}
+            in the selected year and vehicle type
+          </strong>
+          .
         </p>
         <p>
           • <strong className="is-dsb">Dimensions</strong>: a larger size
           represents a higher value (linear scale).
         </p>
         <p>
-          • <strong className="is-dsb">Color scale</strong>: (lowest value) blue
-          - yellow - red (highest value).
+          • <strong className="is-dsb">Color scale</strong>: (lowest value) Blue
+          - Yellow - Red (highest value).
         </p>
       </div>
     </div>

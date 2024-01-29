be2ee5dd7c98c9af70167778f654a0c2ce4d8131
Nicola <ngrassini@yahoo.it>
2024-01-29 12:02:22 +0000

    • add color scale image to chart legend • include two color scale png files

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -1,3 +1,5 @@
+import interpolateRdYlBu from "../assets/images/interpolateRdYlBu.png";
+
 type LegendProps = {
   chartType?: string;
 };
@@ -5,25 +7,36 @@ type LegendProps = {
 export default function Legend({ chartType }: LegendProps) {
   return (
     <div className="pad-5 txt-c txt-14 is-slategrey">
-        <h3 className="is-dsg">Chart Legend</h3>
-        <p>
-          • <strong className="is-dsg">Parameters</strong>: Elements represent
-          the <strong>number of vehicles</strong> recorded at
-          a traffic point{" "}
-          <strong>
-            {" "}
-            in the selected year, per vehicle type
-          </strong>
-          .
-        </p>
-        <p>
-          • <strong className="is-dsg">Dimensions</strong>: a larger size
-          represents a higher value (linear scale).
-        </p>
-        <p>
-          • <strong className="is-dsg">Color scale</strong>: Blue
-          - Yellow - Red (lowest to highest value).
-        </p>
+      <h3 className="is-dsg">Chart Legend</h3>
+      <p>
+        • <strong className="is-dsg">Parameters</strong>: circles represent the{" "}
+        <strong>number of vehicles</strong> recorded at a traffic point{" "}
+        <strong> in the selected year, per vehicle type</strong>.
+      </p>
+      <p>
+        • <strong className="is-dsg">Dimensions</strong>: a larger size
+        represents a higher value (linear scale).
+      </p>
+      <p>
+        • <strong className="is-dsg">Color scale</strong>: Blue - Yellow - Red
+      </p>
+      <div
+        className="w100pc spacedRow txt-13"
+        style={{
+          justifyContent: "center",
+          marginBottom: "5px",
+        }}
+      >
+        <span style={{ paddingRight: "10px" }}>Lowest</span>
+        <img
+          src={interpolateRdYlBu}
+          width="50%"
+          height="5px"
+          style={{ transform: "scaleX(-1)" }}
+          alt="RYB color scale"
+        />
+        <span style={{ paddingLeft: "10px" }}>Highest</span>
+      </div>
     </div>
   );
 }

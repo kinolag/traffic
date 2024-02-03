14a45b8af5520fe61ca41cfd477c3da83ab49a7a
Nicola <ngrassini@yahoo.it>
2024-02-03 10:13:27 +0000

    • update list of areas • add all Inner London boroughs • hide non London areas • set new initial area selection

--- src/assets/data/typesAndConstants.ts
+++ src/assets/data/typesAndConstants.ts
@@ -2,10 +2,19 @@ export type AvailableArea =
   | "gb"
   | "brighton"
   | "bristol"
+  | "camden"
+  | "cityOfLondon"
+  | "cityOfWestminster"
   | "greenwich"
+  | "hackney"
+  | "hammersmithAndFulham"
+  | "islington"
+  | "kensingtonAndChelsea"
+  | "lambeth"
   | "lewisham"
   | "southwark"
-  | "towerHamlets";
+  | "towerHamlets"
+  | "wandsworth";
 
 export type AvailableYear =
   | "2000"
@@ -53,12 +62,21 @@ export type Years = Array<AvailableYear>;
 export type Vehicles = Array<{ value: AvailableVehicle; label: string }>;
 
 export const AREAS: Areas = [
-  { value: "brighton", label: "Brighton" },
-  { value: "bristol", label: "Bristol" },
+  // { value: "brighton", label: "Brighton" },
+  // { value: "bristol", label: "Bristol" },
+  { value: "camden", label: "Camden" },
+  { value: "cityOfLondon", label: "City of London" },
+  { value: "cityOfWestminster", label: "City of Westminster" },
   { value: "greenwich", label: "Greenwich" },
+  { value: "hackney", label: "Hackney" },
+  { value: "hammersmithAndFulham", label: "Hammersmith and Fulham" },
+  { value: "islington", label: "Islington" },
+  { value: "kensingtonAndChelsea", label: "Kensington and Chelsea" },
+  { value: "lambeth", label: "Lambeth" },
   { value: "lewisham", label: "Lewisham" },
   { value: "southwark", label: "Southwark" },
   { value: "towerHamlets", label: "Tower Hamlets" },
+  { value: "wandsworth", label: "Wandsworth" },
 ];
 
 export const YEARS: Years = [
@@ -132,21 +150,88 @@ export const TOPO_MAP_DATA: MapData = {
     objectName: "E06000043",
     center: [-0.105, 50.84],
     scale: 125000,
-  }, 
+  },
   bristol: {
     label: "Bristol",
     mapDataFile: "./data/topoJson/bristol/topo_E06000023.json",
     mapDataFileType: "topoJson",
     objectName: "E06000023",
-    center: [-2.60, 51.485],
+    center: [-2.6, 51.485],
     scale: 125000,
-  }, 
+  },
+  /* Inner London */
+  camden: {
+    label: "Camden",
+    mapDataFile: "./data/topoJson/london/camden/topo_E09000007.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000007",
+    center: [-0.16, 51.54],
+    scale: 170000,
+  },
+  cityOfLondon: {
+    label: "City of London",
+    mapDataFile: "./data/topoJson/london/cityOfLondon/topo_E09000001.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000001",
+    center: [-0.13, 51.51],
+    scale: 170000,
+  },
+  cityOfWestminster: {
+    label: "City of Westminster",
+    mapDataFile: "./data/topoJson/london/cityOfWestminster/topo_E09000033.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000033",
+    center: [-0.14, 51.51],
+    scale: 170000,
+  },
   greenwich: {
     label: "Greenwich",
     mapDataFile: "./data/topoJson/london/greenwich/topo_E09000011.json",
     mapDataFileType: "topoJson",
     objectName: "E09000011",
     center: [0.08, 51.478],
+    scale: 170000,
+  },
+  hackney: {
+    label: "Hackney",
+    mapDataFile: "./data/topoJson/london/hackney/topo_E09000012.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000012",
+    center: [-0.065, 51.55],
+    scale: 170000,
+  },
+  hammersmithAndFulham: {
+    label: "Hammersmith and Fulham",
+    mapDataFile:
+      "./data/topoJson/london/hammersmithAndFulham/topo_E09000013.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000013",
+    center: [-0.21, 51.50],
+    scale: 170000,
+  },
+  islington: {
+    label: "Islington",
+    mapDataFile: "./data/topoJson/london/islington/topo_E09000019.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000019",
+    center: [-0.11, 51.54],
+    scale: 170000,
+  },
+  kensingtonAndChelsea: {
+    label: "Kensington and Chelsea",
+    mapDataFile:
+      "./data/topoJson/london/kensingtonAndChelsea/topo_E09000020.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000020",
+    center: [-0.195, 51.498],
+    scale: 170000,
+  },
+  lambeth: {
+    label: "Lambeth",
+    mapDataFile: "./data/topoJson/london/lambeth/topo_E09000022.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000022",
+    center: [-0.113, 51.46],
     scale: 170000,
   },
   lewisham: {
@@ -173,6 +258,14 @@ export const TOPO_MAP_DATA: MapData = {
     center: [-0.03, 51.515],
     scale: 170000,
   },
+  wandsworth: {
+    label: "Wandsworth",
+    mapDataFile: "./data/topoJson/london/wandsworth/topo_E09000032.json",
+    mapDataFileType: "topoJson",
+    objectName: "E09000032",
+    center: [-0.189, 51.458],
+    scale: 170000,
+  },
 };
 
 export const TRAFFIC_DATA: TrafficData = {
@@ -185,8 +278,33 @@ export const TRAFFIC_DATA: TrafficData = {
   bristol: {
     trafficDataFile: "./data/csv/bristol.csv",
   },
+  /* Inner London */
+  camden: {
+    trafficDataFile: "./data/csv/camden.csv",
+  },
+  cityOfLondon: {
+    trafficDataFile: "./data/csv/cityOfLondon.csv",
+  },
+  cityOfWestminster: {
+    trafficDataFile: "./data/csv/cityOfWestminster.csv",
+  },
   greenwich: {
     trafficDataFile: "./data/csv/greenwich.csv",
+  },
+  hackney: {
+    trafficDataFile: "./data/csv/hackney.csv",
+  },
+  hammersmithAndFulham: {
+    trafficDataFile: "./data/csv/hammersmithAndFulham.csv",
+  },
+  islington: {
+    trafficDataFile: "./data/csv/islington.csv",
+  },
+  kensingtonAndChelsea: {
+    trafficDataFile: "./data/csv/kensingtonAndChelsea.csv",
+  },
+  lambeth: {
+    trafficDataFile: "./data/csv/lambeth.csv",
   },
   lewisham: {
     trafficDataFile: "./data/csv/lewisham.csv",
@@ -196,5 +314,8 @@ export const TRAFFIC_DATA: TrafficData = {
   },
   towerHamlets: {
     trafficDataFile: "./data/csv/tower_hamlets.csv",
+  },
+  wandsworth: {
+    trafficDataFile: "./data/csv/wandsworth.csv",
   },
 };
